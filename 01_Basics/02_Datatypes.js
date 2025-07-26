cd6a6902ec32ca436ef130c0117c2fd89a92336e
Aashish Gangwar <118596350+AashishGangwar@users.noreply.github.com>
2025-07-26 14:12:35 +0000

    datatypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as newer version
+
+// alert() works in the borwser console and we are using node.js 
+
+let name = "Aashish"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => means value not defined
+// symbol => uniqueness
+
+
+console.log(typeof null);
+console.log(typeof undefined);
