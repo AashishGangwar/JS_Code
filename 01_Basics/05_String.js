5ddee9162337f4a252f529059c0d489c4bb35bc6
Aashish Gangwar <118596350+AashishGangwar@users.noreply.github.com>
2025-07-26 20:05:56 +0000

    strings

---
+++ 01_Basics/05_String.js
@@ -0,0 +1,9 @@
+const name = "Aashish"
+const repoCount = 50
+console.log(name + repoCount);
+console.log(`my name is ${name} and repo count is ${repoCount}`);
+
+
+const gameName = new String("Gangwar")
+console.log(gameName[0]);
+console.log(gameName.toUpperCase())
