76735ddc387f7b3bcea370438af87abf56f15ec5
Aashish Gangwar <118596350+AashishGangwar@users.noreply.github.com>
2025-07-26 13:56:05 +0000

    variables code

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,11 @@
+const accountId = 14453
+let accountEmail = "aashish@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+//accountId = 2 changing the value of the variable declared by const is not allowed.
+
+accountEmail = "ag@gmail.com"
+accountPassword = "44332"
+accountCity = "Hyderabad"
+console.table([accountId, accountEmail, accountPassword, accountCity])
