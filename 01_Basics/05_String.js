8e12c014f74139be5c4c1d2eebb238f1e49395c5
Aashish Gangwar <118596350+AashishGangwar@users.noreply.github.com>
2025-07-27 11:11:01 +0000

    Number Maths

--- 01_Basics/05_String.js
+++ 01_Basics/05_String.js
@@ -7,3 +7,22 @@ console.log(`my name is ${name} and repo count is ${repoCount}`);
 const gameName = new String("Gangwar")
 console.log(gameName[0]);
 console.log(gameName.toUpperCase())
+
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('a'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(0, 4);
+console.log(anotherString);
+
+const newStringOne = "   Akash"
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "www.aashish%20gangwar.com"
+console.log(url.replace('%20', '-'))
+
+const myNew = "Aashish-Gangwar-Boss";
+console.log(myNew.split('-'));
