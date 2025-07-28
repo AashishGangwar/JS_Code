4a55f96df554d3e5e41649a2e51afa946e1c6138
Aashish Gangwar <118596350+AashishGangwar@users.noreply.github.com>
2025-07-28 12:43:50 +0000

    array_

--- 02_Basics/01_Arrays.js
+++ 02_Basics/01_Arrays.js
@@ -1,4 +1,27 @@
 const myArr = [1, 2, 4, 5, 6];
 const Heroes = ["Marvel", "DC"];
-const myArr2 = [8,9, 10];
+const myArr2 = [8, 9, 10];
 console.log(myArr[1]);
+
+myArr.push(6)
+myArr.push(7)
+myArr.pop()
+
+console.log(myArr);
+
+myArr.unshift(9)
+myArr.shift()
+
+
+const newArr = myArr.join()
+console.log(myArr);
+console.log(newArr);
+
+console.log("A  ", myArr);
+const myn1 = myArr.slice(1,3);
+
+console.log(myn1);
+console.log("B ", myArr);
+
+const myn2 = myArr.splice(1,3);
+console.log("C ", myArr);
